d328cf114ce2fd9cf588d1a6d3c0e146e5067044
Phuc Nguyen Nho Gia <nngiaphuc0101@gmail.com>
2024-06-26 23:01:42 +0700

    Initial Commit

---
+++ Rendezfood/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rendezfood
+//
+//  Created by Phúc Nguyễn on 26/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rendezfood/RendezfoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  RendezfoodApp.swift
+//  Rendezfood
+//
+//  Created by Phúc Nguyễn on 26/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RendezfoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
